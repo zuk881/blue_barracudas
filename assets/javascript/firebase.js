93e9ec8271d3696434e54f90d1163652a714e6e0
phil cowan <philipjcowan1@gmail.com>
2020-01-26 16:39:11 -0700

    adds some comments

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -12,30 +12,6 @@ firebase.initializeApp(firebaseConfig);
 
 
 //   //code for firebase app authentication
-//   firebase.auth().signInWithRedirect(provider);
-//   firebase.auth().getRedirectResult().then(function(result) {
-//     if (result.credential) {
-//       // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-//       var token = result.credential.accessToken;
-//       // ...
-//     }
-//     // The signed-in user info.
-//     var user = result.user;
-//   }).catch(function(error) {
-//     // Handle Errors here.
-//     var errorCode = error.code;
-//     var errorMessage = error.message;
-//     // The email of the user's account used.
-//     var email = error.email;
-//     // The firebase.auth.AuthCredential type that was used.
-//     var credential = error.credential;
-//     // ...
-//   });firebase.auth().signOut().then(function() {
-//     // Sign-out successful.
-//   }).catch(function(error) {
-//     // An error happened.
-//   });
-
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
