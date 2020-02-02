edce350f571acba27fee069b1a2b741f5ea679a9
Rachel Sipes <rasipes@gmail.com>
2020-02-02 09:41:05 -0500

    testing apply button fix

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -265,7 +265,7 @@ $(document).ready(function () {
             eraseButton.attr("data-url", snapshot.val().url)
             eraseButton.attr("data-id", savedAutoId)
             eraseButton.attr("data-userid", snapshot.val().userid)
-            var appliedButton = $("<a target='_blank'>").addClass("btn-floating btn-large waves-effect waves-light apply-button").html('<i class="material-icons">local_bar</i></a></button>')
+            var appliedButton = $("<a target='_blank'>").addClass("btn-floating btn-large waves-effect waves-light apply-button").text('APPLY HERE')
             appliedButton.attr("href", snapshot.val().url)
             // appliedButton.css("color", "#FFEFD5")
 
